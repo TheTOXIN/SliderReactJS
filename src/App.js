08bcf3d0383c3a85368ad2a2398f1f292be424a3
TheTOXIN <thetoksin@gmail.com>
2018-07-07 23:14:58 +0300

    add header, menu and main

--- src/App.js
+++ src/App.js
@@ -1,44 +1,54 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Grid from 'react-bootstrap/lib/Grid'
-import Row from 'react-bootstrap/lib/Row'
-import Col from 'react-bootstrap/lib/Col'
+import Grid from 'react-bootstrap/lib/Grid';	
 import styled from 'styled-components';
+import Header from './components/Header';
+import Menu from './components/Menu.js';
+import Main from './components/Main.js';
+
+const url = process.env.PUBLIC_URL + '/img/back.png' 
+
+const HeaderWrapper = styled.header`
+	width: 100%;
+	height: 49px;
+	background-color: #242424;
+	opacity: 0.8;
+`
+
+const MenuWrapper = styled.div`
+	height: 89px;
+	padding-top: 21px;
+`
+
+const MainWrapper = styled.main`
+	height: 562px;
+	padding-top: 131px;
+	background: url(${url}) no-repeat;
+	background-size: cover;
+`
 
 class App extends Component {
   render() {
     return (
-        <div>
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Welcome to React</h1>
-          </header>
-          <Hello />
-          <div className="wrapper">
-            <SayFullName name="Yuri" surname="Belousov" link="vk.com/the_toxin" />
-            <SayFullName name="Vlad" surname="Alistratov" link="vk.com/vlad_alv" />
-            <SayFullName name="Kostya" surname="Verechagin" link="vk.com/darklord2504" />
-          </div>
-        </div>
+		<div className="App">
+			<HeaderWrapper>
+				<Grid>
+					<Header />
+				</Grid>
+			</HeaderWrapper>
+			<MenuWrapper>
+				<Grid>
+					<Menu />
+				</Grid>
+			</MenuWrapper>
+			<MainWrapper>
+				<Grid>
+					<Main />
+				</Grid>	
+			</MainWrapper>
+		</div>        
     );
   }
-}
-
-function Hello() {
-  let phrase = "World"
-  return (
-    <h1> Hello {phrase}! </h1>
-  )
-}
-
-function SayFullName(props) {
-  return (
-    <div>
-      <h2> My name {props.name} {props.surname} </h2>
-      <a href = {props.link}> Link on my contacts </a>
-    </div>
-  )
 }
 
 export default App;
